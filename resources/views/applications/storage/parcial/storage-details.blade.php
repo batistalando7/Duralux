8fb9026c871075488da49f280856b4f9bb177c2c
JoelPaixao18 <joelpaixaopaixao93@gmail.com>
2025-07-24 16:22:54 +0100

    Recorte do menu Applications

---
+++ resources/views/applications/storage/parcial/storage-details.blade.php
@@ -0,0 +1,252 @@
+    <!--! ================================================================ !-->
+    <!--! BEGIN: File or Folder  Details [file-manager-folder-details] !-->
+    <!--! ================================================================ !-->
+    <div class="offcanvas offcanvas-end file-manager-folder-details" tabindex="-1" id="fileFolderDetailsOffcanvas">
+        <div class="offcanvas-header border-bottom">
+            <h5 class="offcanvas-title" id="staticBackdropLabel">Storage Details</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body p-0">
+            <div class="tab-content">
+                <div class="tab-pane fade active show" id="fileDetailsTab">
+                    <div class="p-4 d-flex align-items-center justify-content-center">
+                        <img src="assets/images/storage-icons/undefined.png" class="img-fluid wd-200" alt="">
+                    </div>
+                    <!--! BEGIN: [drive-specifications] !-->
+                    <div class="drive-specifications">
+                        <div class="px-4 py-2 fw-bold text-dark border-top border-bottom sticky-top bg-gray-100">Specifications</div>
+                        <div class="p-4 fs-12">
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Type</span>
+                                <span class="text-muted">Folder</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Location</span>
+                                <span class="text-muted">Files & Documents</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Contributing</span>
+                                <span class="text-muted">Specify</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Album</span>
+                                <span class="text-muted">Specify</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Genre</span>
+                                <span class="text-muted">Specify</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Year</span>
+                                <span class="text-muted">2021</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Size</span>
+                                <span class="text-muted">65.52 MB</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Track</span>
+                                <span class="text-muted">Specify</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Artist</span>
+                                <span class="text-muted">Specify</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Title</span>
+                                <span class="text-muted">All Folder</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Bit Rate</span>
+                                <span class="text-muted">512kbps</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Date Modification</span>
+                                <span class="text-muted">10/10/2022</span>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="fw-semibold text-dark">Date Created</span>
+                                <span class="text-muted">05/05/2022</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--! BEGIN: [drive-settings] !-->
+                    <div class="drive-settings">
+                        <div class="px-4 py-2 fw-bold text-dark border-top border-bottom sticky-top bg-gray-100">Settings</div>
+                        <div class="p-4">
+                            <div class="form-group mb-4">
+                                <div class="form-item custom-control custom-checkbox checkbox-primary">
+                                    <input type="checkbox" class="custom-control-input" id="checkboxSynchronization">
+                                    <label class="custom-control-label ps-2" for="checkboxSynchronization">
+                                        <span class="fw-semibold" style="margin-top: -5px">Synchronization</span>
+                                        <span class="fs-11 fw-normal text-muted text-truncate-1-line">Lorem ipsum quia dolor sit amet adipisci quia dolor</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <div class="form-item custom-control custom-checkbox checkbox-primary">
+                                    <input type="checkbox" class="custom-control-input" id="checkboxAutoBackup" checked>
+                                    <label class="custom-control-label ps-2" for="checkboxAutoBackup">
+                                        <span class="fw-semibold" style="margin-top: -5px">Auto Backup</span>
+                                        <span class="fs-11 fw-normal text-muted text-truncate-1-line">Lorem ipsum quia dolor sit amet adipisci quia dolor</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <div class="form-item custom-control custom-checkbox checkbox-primary">
+                                    <input type="checkbox" class="custom-control-input" id="checkboxMuteNotification">
+                                    <label class="custom-control-label ps-2" for="checkboxMuteNotification">
+                                        <span class="fw-semibold" style="margin-top: -5px">Mute Notification</span>
+                                        <span class="fs-11 fw-normal text-muted text-truncate-1-line">Lorem ipsum quia dolor sit amet adipisci quia dolor</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <div class="form-item custom-control custom-checkbox checkbox-primary">
+                                    <input type="checkbox" class="custom-control-input" id="checkboxPushNotification" checked>
+                                    <label class="custom-control-label ps-2" for="checkboxPushNotification">
+                                        <span class="fw-semibold" style="margin-top: -5px">Push Notification</span>
+                                        <span class="fs-11 fw-normal text-muted text-truncate-1-line">Lorem ipsum quia dolor sit amet adipisci quia dolor</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <div class="form-item custom-control custom-checkbox checkbox-primary">
+                                    <input type="checkbox" class="custom-control-input" id="checkboxGetNotification">
+                                    <label class="custom-control-label ps-2" for="checkboxGetNotification">
+                                        <span class="fw-semibold" style="margin-top: -5px">Get Notification</span>
+                                        <span class="fs-11 fw-normal text-muted text-truncate-1-line">Lorem ipsum quia dolor sit amet adipisci quia dolor</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--! BEGIN: [drive-status] !-->
+                    <div class="drive-status">
+                        <div class="px-4 py-2 fw-bold text-dark border-top border-bottom sticky-top bg-gray-100">Statistics</div>
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between align-items-center mb-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-text bg-soft-primary text-primary border-soft-primary rounded-3">
+                                        <i class="feather-image"></i>
+                                    </div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold text-dark">Images</div>
+                                        <span class="fs-11 fw-normal text-muted">1,252 Files</span>
+                                    </div>
+                                </div>
+                                <div class="badge bg-gray-100 text-dark">2.25 GB</div>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-text bg-soft-danger text-danger border-soft-danger rounded-3">
+                                        <i class="feather-video"></i>
+                                    </div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold text-dark">Video Files</div>
+                                        <span class="fs-11 fw-normal text-muted">3,586 Files</span>
+                                    </div>
+                                </div>
+                                <div class="badge bg-gray-100 text-dark">9.75 GB</div>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-text bg-soft-success text-success border-soft-success rounded-3">
+                                        <i class="feather-file-text"></i>
+                                    </div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold text-dark">Documents</div>
+                                        <span class="fs-11 fw-normal text-muted">2,147 Files</span>
+                                    </div>
+                                </div>
+                                <div class="badge bg-gray-100 text-dark">3.47 GB</div>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-text bg-soft-teal text-teal border-soft-teal rounded-3">
+                                        <i class="feather-music"></i>
+                                    </div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold text-dark">Audio Files</div>
+                                        <span class="fs-11 fw-normal text-muted">2,354 Files</span>
+                                    </div>
+                                </div>
+                                <div class="badge bg-gray-100 text-dark">3.49 GB</div>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-text bg-soft-warning text-warning border-soft-warning rounded-3">
+                                        <i class="feather-monitor"></i>
+                                    </div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold text-dark">UI/UX Design</div>
+                                        <span class="fs-11 fw-normal text-muted">3,456 Files</span>
+                                    </div>
+                                </div>
+                                <div class="badge bg-gray-100 text-dark">2.94 GB</div>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-4">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-text bg-soft-info text-info border-soft-info rounded-3">
+                                        <i class="feather-layers"></i>
+                                    </div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold text-dark">Other Files</div>
+                                        <span class="fs-11 fw-normal text-muted">2,258 Files</span>
+                                    </div>
+                                </div>
+                                <div class="badge bg-gray-100 text-dark">2.00 GB</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--! BEGIN: [drive-comments] !-->
+                    <div class="drive-comments">
+                        <div class="px-4 py-2 fw-bold text-dark border-top border-bottom sticky-top bg-gray-100">Comments</div>
+                        <div class="p-4">
+                            <div class="mb-4 d-flex align-items-start">
+                                <div class="me-3 avatar-image">
+                                    <img src="assets/images/avatar/3.png" class="rounded-circle img-fluid" alt="image">
+                                </div>
+                                <div>
+                                    <div class="d-flex align-items-center">
+                                        <a href="javascript:void(0)" class="me-2"> Marianne Audrey </a>
+                                        <div class="fs-11 fw-normal text-muted">(Mar 2, 5:26 am)</div>
+                                    </div>
+                                    <div class="fs-12 text-muted mt-1 text-truncate-2-line">Lorem ipsum dolor sit amet, consec tetuer adipi scing elit aenean commodo scing elit aenean commodo</div>
+                                </div>
+                            </div>
+                            <div class="mb-4 d-flex align-items-start">
+                                <div>
+                                    <div class="me-3 bg-warning text-white avatar-text">N</div>
+                                </div>
+                                <div>
+                                    <div class="d-flex align-items-center">
+                                        <a href="javascript:void(0)" class="me-2"> Nancy Elliot </a>
+                                        <div class="fs-11 fw-normal text-muted">(Mar 2, 5:26 am)</div>
+                                    </div>
+                                    <div class="fs-12 text-muted mt-1 text-truncate-2-line">Lorem ipsum dolor sit amet, consec tetuer adipi scing elit aenean commodo scing elit aenean commodo</div>
+                                </div>
+                            </div>
+                            <div class="mb-4 d-flex align-items-start">
+                                <div class="me-3 avatar-image">
+                                    <img src="assets/images/avatar/4.png" class="rounded-circle img-fluid" alt="image">
+                                </div>
+                                <div>
+                                    <div class="d-flex align-items-center">
+                                        <a href="javascript:void(0)" class="me-2"> Holland Scott </a>
+                                        <div class="fs-11 fw-normal text-muted">(Mar 2, 2:21 pm)</div>
+                                    </div>
+                                    <div class="fs-12 text-muted mt-1 text-truncate-2-line">Lorem ipsum dolor sit amet, consec tetuer adipi scing elit aenean commodo scing elit aenean commodo</div>
+                                </div>
+                            </div>
+                            <textarea rows="5" class="form-control" placeholder="Comment"></textarea>
+                            <a href="javascript:void(0);" class="btn btn-primary mt-4">Post Comment</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--! ================================================================ !-->
+    <!--! END: File  or Folder  Details [file-manager-folder-details] !-->
+    <!--! ================================================================ !-->
